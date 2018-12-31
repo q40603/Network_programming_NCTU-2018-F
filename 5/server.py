b46a8be3b04c1409a4cdbedaa9c7d13ce3daa0a8
q40603 <q40603@gmail.com>
2018-12-31 21:26:18 +0800

    change port

--- 5/server.py
+++ 5/server.py
@@ -7,11 +7,11 @@ import stomp
 
 
 class DBControl(object):
-    def __init__(self, mq_ip='127.0.0.1', mq_port=61616):
+    def __init__(self):
         try:
-            self.mq = stomp.Connection([(mq_ip, 61616)])
+            self.mq = stomp.Connection()
             self.mq.start()
-            self.mq.connect(wait=True)
+            self.mq.connect('admin', 'password', wait=True)
         except Exception as e:
             print(e, file=sys.stderr)
 
